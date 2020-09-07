7426d5c0d6019bee9d479c7884327511eb12c639
alan-serrano <alan.serrano.pch@gmail.com>
2020-09-07 07:56:48 -0400

    Updates

---
+++ precache-manifest.34555500e51e93cad0b03844c0a38423.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1051733891cf805e7ac3f2525f532e94",
+    "url": "/%7Brepo-name%7D/index.html"
+  },
+  {
+    "revision": "d8f4643d93fdbe8beca3",
+    "url": "/%7Brepo-name%7D/static/css/main.66d1dcdc.chunk.css"
+  },
+  {
+    "revision": "d8bab78631ca80276d6d",
+    "url": "/%7Brepo-name%7D/static/js/2.e0a7a1c5.chunk.js"
+  },
+  {
+    "revision": "7c6de3322eb4ea2ab32454492afd4f37",
+    "url": "/%7Brepo-name%7D/static/js/2.e0a7a1c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8f4643d93fdbe8beca3",
+    "url": "/%7Brepo-name%7D/static/js/main.165ee8fc.chunk.js"
+  },
+  {
+    "revision": "ad11187455033893a457",
+    "url": "/%7Brepo-name%7D/static/js/runtime-main.0ef58934.js"
+  }
+]);
